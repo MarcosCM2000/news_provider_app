efa868c8ee6137464bad8d5604267073580a798e
MarcosCInland <marcos.andres@inlandlogistics.co>
2023-02-06 10:08:35 -0600

    bottom navigation bar

--- lib/pages/tabs_page.dart
+++ lib/pages/tabs_page.dart
@@ -1,20 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class TabsPage extends StatelessWidget {
   const TabsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: PageView(
-          physics: BouncingScrollPhysics(),
-          children: <Widget>[
-            Container(color: Colors.red),
-            Container(color: Colors.green),
-          ],
-        ),
+    return ChangeNotifierProvider(
+      create: (_) => _NavegacionModel(),
+      child: const Scaffold(
+        body: Center(child: _Paginas()),
+        bottomNavigationBar: _Navegacion(),
       ),
     );
+  }
+}
+
+class _Navegacion extends StatelessWidget {
+  const _Navegacion({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final navegacionModel = Provider.of<_NavegacionModel>(context);
+    return BottomNavigationBar(
+        currentIndex: navegacionModel.paginaActual,
+        onTap: (value) => navegacionModel.paginaActual = value,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline), label: 'Para ti'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.public), label: 'Encabezados'),
+        ]);
+  }
+}
+
+class _Paginas extends StatelessWidget {
+  const _Paginas({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final navegacionModel = Provider.of<_NavegacionModel>(context);
+    return PageView(
+      physics: const NeverScrollableScrollPhysics(),
+      children: <Widget>[
+        Container(color: Colors.red),
+        Container(color: Colors.green),
+      ],
+    );
+  }
+}
+
+class _NavegacionModel extends ChangeNotifier {
+  int _paginaActual = 0;
+
+  int get paginaActual => _paginaActual;
+
+  set paginaActual(int valor) {
+    _paginaActual = valor;
+    notifyListeners();
   }
 }
